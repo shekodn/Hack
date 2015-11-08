98487c79fbc4c308c65e34335b3e711ab7d7911b
Maria Paula <ma.paula.anastas@gmail.com>
2015-11-08 01:32:31 -0600

    Log In Toast
    Video antes del INTENT todo bien

--- app/src/main/java/com/example/mariapaulaanastas/mesero/Register_Activity.java
+++ app/src/main/java/com/example/mariapaulaanastas/mesero/Register_Activity.java
@@ -7,6 +7,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 public class Register_Activity extends ActionBarActivity {
 
 
@@ -30,9 +34,46 @@ public class Register_Activity extends ActionBarActivity {
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // Toast.makeText(Register_Activity.this, "Bienvenido", Toast.LENGTH_LONG).show();
+                //get the user name password and email and convert it to String
+
+                String username = mUsername.getText().toString().trim();
+                String email = mUserEmail.getText().toString().trim();
+                String password = mUserPassword.getText().toString().trim();
+
+
+                //Store in parse
+                ParseUser user = new ParseUser();
+                user.setUsername(username);
+                user.setPassword(password);
+                user.setEmail(email);
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                      if(e == null){
+                          //success
+                          Toast.makeText(Register_Activity.this, "Bienvenido", Toast.LENGTH_LONG).show();
+
+                          //take to home page post login
+
+                      }
+
+                        else{
+                          //error in sign up
+
+
+
+                      }
+                    }
+                });
+
+
+
+
             }
         });
+
+
 
 
 
